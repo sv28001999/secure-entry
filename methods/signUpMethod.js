b013387f48598e7056bbf88b1a2693c231675cdf
unknown <shivam033vishwakarma@gmail.com>
2024-05-05 21:21:55 +0530

    Login Account Change

--- methods/signUpMethod.js
+++ methods/signUpMethod.js
@@ -59,12 +59,12 @@ const sendMail = async (otp, emailId) => {
 
 async function generateUniqueCode(string1, string2, string3) {
     const timestamp = Date.now().toString(36); // Convert current timestamp to base36 string
-    const random = Math.random().toString(36).substring(2, 5); // Generate a random base36 string
+    const random = Math.random().toString(36).substr(2, 5); // Generate a random base36 string
 
     // Concatenate the three strings and append timestamp and random string
-    const uniqueCode = string1.substring(0, 2) +
-        string2.substring(0, 3) +
-        string3.substring(0, 3) +
+    const uniqueCode = string1.substr(0, 2) +
+        string2.substr(0, 3) +
+        string3.substr(0, 3) +
         timestamp +
         random;
 
